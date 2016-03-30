7159caebd0ed515ecc5ad7ac007bb77d7e86518d
Fuwei Li <liveysmile@gmail.com>
2016-03-30 16:06:39 +0800

    Update logistic_regression_vec.m

--- ex1/logistic_regression_vec.m
+++ ex1/logistic_regression_vec.m
@@ -19,3 +19,7 @@ function [f,g] = logistic_regression_vec(theta, X,y)
   %        Store the objective function value in 'f', and the gradient in 'g'.
   %
 %%% YOUR CODE HERE %%%
+temp = theta'*X;
+h=1./(1+exp(-temp));
+f=-y*log(h)'-(1-y)*log(1-h)';
+g=X*(h-y)';
