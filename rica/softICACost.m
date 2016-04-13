bdf48f8998177b38692c2f23faa293cb8e15bc1c
Fuwei Li <liveysmile@gmail.com>
2016-04-13 22:45:44 +0800

    Update softICACost.m

--- rica/softICACost.m
+++ rica/softICACost.m
@@ -9,7 +9,14 @@ Wold = W;
 W = l2rowscaled(W, 1);
 
 %%% YOUR CODE HERE %%%
+epsilong=1e-2;
+y=W*x;
+cost= params.lambda*sum(sum(sqrt(y.^2+epsilong)))+...
+      .5*norm(W'*W*x-x,'fro')^2;
 
+gd1=(y./sqrt(y.^2+epsilong))*x';
+gd2=2*W*(W'*y-x)*x'+2*y*(W'*y-x)';
+Wgrad =params.lambda*gd1+.5*gd2;  
 % unproject gradient for minFunc
 grad = l2rowscaledg(Wold, W, Wgrad, 1);
 grad = grad(:);
